41f548bcd171534e800e3ee4b22ce016b68e0877
Juliana Leme <julianaleme@gmail.com>
2019-11-12 09:04:59 -0300

    Primeiro Commit 20191112

---
+++ dist/precache-manifest.cf7a38966fd29f0a90b938f419a221b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a279d39b3507e0c99b30",
+    "url": "/css/chunk-07a0f5e8.2f459f28.css"
+  },
+  {
+    "revision": "214da4b5b14d03ff1719",
+    "url": "/css/chunk-16145b54.4641f3cc.css"
+  },
+  {
+    "revision": "af56487d3e1e25625537",
+    "url": "/css/chunk-c3e1b912.cb7d901a.css"
+  },
+  {
+    "revision": "6f38023c379b15f9e0ae",
+    "url": "/css/chunk-vendors.d77b2d23.css"
+  },
+  {
+    "revision": "094e8109ac0f42a37ebba5b5050bddef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ba5778ee88c67d0f797",
+    "url": "/js/app.bf086c0c.js"
+  },
+  {
+    "revision": "a279d39b3507e0c99b30",
+    "url": "/js/chunk-07a0f5e8.f5f21fe2.js"
+  },
+  {
+    "revision": "214da4b5b14d03ff1719",
+    "url": "/js/chunk-16145b54.400e7153.js"
+  },
+  {
+    "revision": "af56487d3e1e25625537",
+    "url": "/js/chunk-c3e1b912.71b2f813.js"
+  },
+  {
+    "revision": "6f38023c379b15f9e0ae",
+    "url": "/js/chunk-vendors.16a1b81a.js"
+  },
+  {
+    "revision": "7810725a6e12d59f732df9d439eba169",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
